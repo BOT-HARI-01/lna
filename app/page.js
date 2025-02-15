e0618557c615c6f8c46a9e37f2ccbce6483cc635
HARI VINAYAK <122667299+BOT-HARI-01@users.noreply.github.com>
2025-02-15 21:40:23 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -3,7 +3,6 @@ import ComponentContainer from "@/components/ComponentContainer";
 export default function Home() {
 return (
     <div>
-        //asdfg
       <ComponentContainer />
     </div>
   );
